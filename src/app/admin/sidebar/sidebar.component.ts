e4040fc7f99d39f41b42992411d817829a77efc2
Atuti Michelle <michelle.atuti@strathmore.edu>
2025-02-13 11:41:06 +0300

    Add: ✅Creation of sidebar component

---
+++ src/app/admin/sidebar/sidebar.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CoreService } from '../../services/core.service'; 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser, faBell, faTachometerAlt, faCalendarAlt, faEdit, faSignOutAlt, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone:true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, FontAwesomeModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+
+
+export class SidebarComponent implements OnInit {
+  faUser = faUser;
+  faBell = faBell;
+  faTachometerAlt = faTachometerAlt;
+  faCalendarAlt = faCalendarAlt;
+  faEdit = faEdit;
+  faSignOutAlt = faSignOutAlt;
+  faMagnifyingGlass = faMagnifyingGlass;
+
+
+  username: string = '';
+  userId: number | null = null;
+  errorMessage:string='';
+  
+
+  constructor(private coreService: CoreService) {}
+
+  ngOnInit() {
+    this.coreService.getUserDetails().subscribe(
+      response => {
+        console.log('✅ User Details:', response);
+        if (!response || !response.id) {
+          this.errorMessage = 'User details not found. Please log in again.';
+          return;
+        }
+        this.userId = response.id;
+        this.username = response.name;
+        // this.loadRegisteredEvents();
+      },
+      error => {
+        console.error('❌ Error fetching user details:', error);
+        this.errorMessage = 'Error fetching user details. Please log in again.';
+      }
+    );
+  }
+  logout(event: Event) {
+    event.preventDefault(); 
+    this.coreService.logout(); 
+    window.location.href = '../'; 
+  }
+  }
